e8e3da352c58ba30de4b046179629a4be087e5ca
VAZ-2107 <94621971+BAZ-2107@users.noreply.github.com>
2026-04-02 14:37:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from PyQt5.QtGui import *
 from PyQt5 import uic
 import sys
 import os
+import json
+import time
 import shutil
+from datetime import datetime
 
 
 class Window(QMainWindow):
@@ -21,36 +24,82 @@ class Window(QMainWindow):
         self.arr = []
         self.show_author = None
         self.show_help = None
+        self.show_save_system = None
+
+        self.save_system_action.triggered.connect(self.__open_save_system)
+        self.load_system_action.triggered.connect(self.__load_system)
+
         self.about_action.triggered.connect(self.__open_author)
         self.help_action.triggered.connect(self.__open_help)
         self.add_string.clicked.connect(self.__add_line)
         self.del_string.clicked.connect(self.__del_line)
+        self.__load_system(True)
 
     def __open_author(self):
-        self.show_author = Author(self)
+        self.show_author = Author()
         self.show_author.show()
 
     def __open_help(self):
-        self.show_help = Help(self)
+        self.show_help = Help()
         self.show_help.show()
+
+    def __load_system(self, for_run=False):
+        if for_run:
+            answer = "user_files/default.json"
+        else:
+            answer, _ = QFileDialog.getOpenFileName(self, "Выбор файла", "user_files", "JSON Files (*.json)")
+        if answer:
+            try:
+                with open(answer, 'r') as fille:
+                    data = json.load(fille)
+                n = len(data)
+                self.table.setRowCount(0)
+                for i in range(n):
+                    self.__add_line()
+                    st_i = str(i)
+                    self.table.setItem(i, 1, QTableWidgetItem(data[st_i][0]))
+                    self.table.setItem(i, 3, QTableWidgetItem(data[st_i][1]))
+                self.statusbar.showMessage(f"Система из файла {answer} успешно загружена")
+            except Exception as m:
+                self.statusbar.showMessage(f"Не удалось обработать файл: {answer}")
 
     def __add_line(self):
         n = self.table.rowCount()
         self.table.insertRow(n)
-        item = QTableWidgetItem(f"x_{n + 1}")
-        item2 = QTableWidgetItem(f"f_{n + 1}(x)")
-        self.table.setItem(n, 0, item)
+        item1 = QTableWidgetItem(f"dx{n + 1}(t)/dt")
+        item2 = QTableWidgetItem(f"f{n + 1}(x)")
+        item3 = QTableWidgetItem(f"x{n + 1}(t=t_0)")
+        item4 = QTableWidgetItem(f"g{n + 1}(x)")
+        self.table.setItem(n, 0, item1)
+        self.table.item(n, 0).setFlags(self.table.item(n, 0).flags() & ~Qt.ItemIsEditable)
         self.table.setItem(n, 1, item2)
+        self.table.setItem(n, 2, item3)
+        self.table.item(n, 2).setFlags(self.table.item(n, 2).flags() & ~Qt.ItemIsEditable)
+        self.table.setItem(n, 3, item4)
+        self.statusbar.showMessage(f"Добавлена {n + 1}-я строка")
 
     def __del_line(self):
         n = self.table.rowCount()
         if n > 0:
             self.table.removeRow(n - 1)
+            self.statusbar.showMessage(f"Удалена {n}-я строка")
 
-    def __save_system(self):
+    def __open_save_system(self):
         n = self.table.rowCount()
-        if n:
-            pass
+        if n > 0:
+            self.show_save_system = SaveSystem(self)
+            self.show_save_system.show()
+
+    def saving_system(self):
+        n = self.table.rowCount()
+        arr = dict((i, [self.table.item(i, 1).text(), self.table.item(i, 3).text()]) for i in range(n))
+        st = str(datetime.now())
+        st = st[:st.find('.')].replace(':', '.')
+        path, _ = QFileDialog.getSaveFileName(self, "Сохранить", f"user_files/{st}.json", "JSON Files (*.json)")
+        if path:
+            with open(path, "w") as fille:
+                json.dump(arr, fille)
+            self.statusbar.showMessage(f"Система была сохранена в файл: {path}")
 
     def keyPressEvent(self, event):
         if event.matches(QKeySequence.Close):
@@ -59,10 +108,12 @@ class Window(QMainWindow):
             self.__add_line()
         elif event.matches(QKeySequence.Cut):
             self.__del_line()
+        elif event.matches(QKeySequence.Open):
+            self.__load_system()
         elif event.matches(QKeySequence.Undo):
             self.__open_author()
         elif event.matches(QKeySequence.Save):
-            self.__save_system()
+            self.__open_save_system()
 
 
 class Author(QWidget):
@@ -91,6 +142,41 @@ class Help(QWidget):
     def keyPressEvent(self, event):
         if event.matches(QKeySequence.Close):
             self.close()
+
+
+class SaveSystem(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        uic.loadUi("ui/for_save_system.ui", self)
+        self.setupUI(*args, **kwargs)
+
+    def setupUI(self, *args, **kwargs):
+        self.parent = args[0]
+        self.parent.setDisabled(True)
+        self.yes.clicked.connect(self.button_pressed)
+        self.no.clicked.connect(self.button_pressed)
+        self.__fill_table()
+
+    def __fill_table(self):
+        table = self.parent.table
+        n = table.rowCount()
+        self.tablle.setRowCount(n)
+        for i in range(n):
+            item1 = QTableWidgetItem(table.item(i, 1).text())
+            self.tablle.setItem(i, 0, item1)
+            item2 = QTableWidgetItem(table.item(i, 3).text())
+            self.tablle.setItem(i, 1, item2)
+            self.tablle.item(i, 0).setFlags(item1.flags() & ~Qt.ItemIsEditable)
+            self.tablle.item(i, 1).setFlags(item2.flags() & ~Qt.ItemIsEditable)
+
+    def button_pressed(self):
+        sender = self.sender()
+        if sender and (sender.text() == "Сохранить"):
+            self.parent.saving_system()
+        self.close()
+
+    def closeEvent(self, event):
+        self.parent.setEnabled(True)
 
 
 # catch errors
